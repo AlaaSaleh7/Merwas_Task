61e274589139abc65ed8b161bc2ad2d8d6002a30
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-09-23 21:40:08 +0300

    Navbar 2

--- MerwasApp/src/App.js
+++ MerwasApp/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Nav from './componants/Navegation'
+import Home from './componants/Home'
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Home/>
     </div>
     
   );
---
+++ MerwasApp/src/componants/Home.js
@@ -0,0 +1,15 @@
+import './Home.css'
+function Home() {
+    return (
+      <div className="Home">
+       <img src="https://mediacdn.grabone.co.nz/asset/yDjZTGuDg1" class="desktopheaderimage" alt="Spring Collection"></img>
+       <p> <span>Covid 19 Update:</span>Covid 19 Update: We encourage you to shop as normal for all your products but shipping delays may occur. Stay safe NZ!</p>
+      </div>
+      
+    );
+  }
+  
+  export default Home;
+
+
+
